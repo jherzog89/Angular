39683abfb5774c043a170981d301393943035bcc
Jason Herzog <jasonrherzog@gmail.com>
2025-07-29 13:41:56 -0500

    Adding API gateway and naming server

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = "http://localhost:8080"
 export const TODO_JPA_API_URL = "http://localhost:8080/jpa"
-export const REVERSE_STRING_URL = "http://localhost:8080/reverseString"
+export const REVERSE_STRING_URL = "http://localhost:8765/REVERSE-STRING-SERVICE/reverseString"
